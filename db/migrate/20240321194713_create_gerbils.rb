b34b91f35f124c548f9134890577cb8d4bd32748
Aurora Sanderson <AuroraTuring@gmail.com>
2024-03-22 14:17:46 -0600

    Remove competition_id to allow migrate to add it

--- db/migrate/20240321194713_create_gerbils.rb
+++ db/migrate/20240321194713_create_gerbils.rb
@@ -1,7 +1,6 @@
 class CreateGerbils < ActiveRecord::Migration[7.1]
   def change
     create_table :gerbils do |t|
-      t.integer :competition_id
       t.string :name
       t.string :color
       t.boolean :neutered
