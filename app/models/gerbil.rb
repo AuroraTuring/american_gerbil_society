e47649370cf3e3f4e841980229936941fa1d0f5a
Aurora Sanderson <AuroraTuring@gmail.com>
2024-03-26 16:59:05 -0600

    Edit display information on index

---
+++ app/models/gerbil.rb
@@ -0,0 +1,3 @@
+class Gerbil < ApplicationRecord
+
+end
--- spec/features/gerbils/index_spec.rb
+++ spec/features/gerbils/index_spec.rb
@@ -8,9 +8,11 @@ RSpec.describe "competitions index page", type: :feature do
   describe "as a visitor" do
     describe "when I visit '/gerbils'" do
       it "then I see each Gerbil in the system including the Gerbil's attributes" do
-        crystal = Gerbil.create!(name: "Crystal", color: "Mottled Lilac", age: 2.5, neutered: false)
-        jasper = Gerbil.create!(name: "Jasper", color: "Polar Fox", age: 3, neutered: false)
-        onyx = Gerbil.create!(name: "Onyx", color: "Black", age: 1, neutered: true)
+        new_england = Competition.create!(name: "New England Show", location: "Ohio", date: "04/01/2024", admission_paid: true, prize_money: 250)
+        south_east = Competition.create!(name: "South East Show", location: "Florida", date: "01/01/2024", admission_paid: true, prize_money: 250)
+        crystal = Gerbil.create!(competition_id: new_england.id, name: "Crystal", color: "Mottled Lilac", age: 2.5, neutered: false)
+        jasper = Gerbil.create!(competition_id: new_england.id, name: "Jasper", color: "Polar Fox", age: 3, neutered: false)
+        onyx = Gerbil.create!(competition_id: south_east.id, name: "Onyx", color: "Black", age: 1, neutered: true)
 
         visit "/gerbils"
       end
